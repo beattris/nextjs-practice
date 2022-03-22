250d7fd514696f373fbbbdd7f8eb5b820d312abb
Beattris <beatriceebirim@gmail.com>
2022-03-22 14:11:19 +0100

    extracted dynamic parameter values

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,6 +1,10 @@
+import { useRouter } from "next/router";
+
 function NewsDetailPage() {
-    return <h1>The News Detail Page</h1>;
-  }
-  
-  export default NewsDetailPage;
-  
+  const router = useRouter();
+  const newsId = router.query.newsId;
+  console.log(router.query.newsId);
+  return <h1>The News Detail Page</h1>;
+}
+
+export default NewsDetailPage;
