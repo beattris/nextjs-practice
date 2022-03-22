a0e3b0d06ce17961fa87fb3e3963ec721d80121b
Beattris <beatriceebirim@gmail.com>
2022-03-22 14:25:09 +0100

    added a link between the pages using the Link component

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,5 +1,16 @@
+import Link from "next/link";
+import { Fragment } from "react";
+
 function NewsPage() {
-  return <h1>The News Page</h1>;
+  return (
+    <Fragment> 
+      <h1>The News Page</h1>
+      <ul>
+        <li><Link href='/news/nejs-is-cool'>NextJS is cool</Link></li>
+        <li><Link href='/news/nanother-article-is-cool'>Another news article.</Link></li>
+      </ul>
+    </Fragment>
+  );
 }
 
 export default NewsPage;
